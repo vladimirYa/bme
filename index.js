0cb982d9829a816681a64b464f9af103d8a7ec74
artemandrosov <artem.androsov.dev@gmail.com>
2023-09-12 00:41:54 +0200

    adjust styles, create contact dialog

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ function handleIntersection(entries) {
 
 const observer = new IntersectionObserver(handleIntersection);
 document.querySelectorAll('.animated').forEach(section => observer.observe(section));
+
+
+function openContactDialog() {
+    const dialogEl = document.getElementById('contact-overlay');
+    dialogEl.classList.add('visible');
+    const backdropEl = document.createElement('div');
+    backdropEl.id = 'overlay-backdrop';
+    backdropEl.onclick = () => closeContactDialog();
+    dialogEl.appendChild(backdropEl);
+    setTimeout(() => backdropEl.classList.add('shown'));
+}
+
+function closeContactDialog() {
+    const dialogEl = document.getElementById('contact-overlay');
+    const backdropEl = document.getElementById('overlay-backdrop');
+    dialogEl.removeChild(backdropEl)
+    dialogEl.classList.remove('visible');
+    backdropEl.classList.remove('shown');
+}
